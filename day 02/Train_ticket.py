0ee7d4634be17f6c596678eeea10cea55f3b0fae
vinayakd282007-rgb <vinayakd282007@gmail.com>
2026-07-03 02:02:31 +0530

    Added day 02 programs

---
+++ day 02/Train_ticket.py
@@ -0,0 +1,19 @@
+name = input("Passenger name: ")
+age = input("Passenger age: " )
+from_city = input("From city: ")
+to_city = input("To city: ")
+train_name = input("Train name: ")
+seat_no = input("Seat no: ")
+print(f"""
+======================================
+             Train ticket 
+======================================
+Passenger name: {name}
+Passenger age: {age}
+From city: {from_city}
+To city: {to_city}
+Train name: {train_name}
+Seat no: {seat_no} 
+=========================================
+"Have a safe journey" !
+""")
