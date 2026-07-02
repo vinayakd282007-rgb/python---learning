7f17193d3e5ab56cf15b6c16cf95aac5e5e3837b
vinayakd282007-rgb <vinayakd282007@gmail.com>
2026-07-02 23:53:58 +0530

    day 01

--- day 01/day 02/atm_balance_checker.py
+++
@@ -1,8 +0,0 @@
-balance=float(input("Enter your balance: "))
-withdraw=float(input("Enter your withdraw amount: "))
-if balance>=withdraw:
-    balance=balance-withdraw
-    print("Your remaining balance is: ",balance)
-    print("Withdraw successful!")
-else:
-        print("Insufficient balance!")
