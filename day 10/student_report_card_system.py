ead0d3e1eba0a3f2a3dfcd5f1f2d6b79d4514dc9
vinayakd282007-rgb <vinayakd282007@gmail.com>
2026-07-10 23:04:44 +0530

    python for loops

---
+++ day 10/student_report_card_system.py
@@ -0,0 +1,9 @@
+students = {
+    "Rahul": 85,
+    "Anjali": 92,
+    "Kiran": 34,
+    "Sneha": 76,
+    "Amit": 49
+}
+for student , marks in enumerate students.items():
+    print(f"=================== STUDENT REPORT =========================== \n {student} = {marks}")
