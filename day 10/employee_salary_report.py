65b7817434515d888d0c7c55385b8ce382098a58
vinayakd282007-rgb <vinayakd282007@gmail.com>
2026-07-10 20:55:35 +0530

    python for loops

---
+++ day 10/employee_salary_report.py
@@ -0,0 +1,11 @@
+employees = {
+    "Amit": 50000,
+    "Neha": 62000,
+    "Ravi": 45000,
+    "Priya": 70000,
+    "Karan": 52000
+}
+for employee , salary in employees.items():
+     if salary>50000:
+        print(f"{employee} earns ₹{salary}")
+        
