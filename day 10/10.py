347f1e9c98f02e83c54eb01bd10a3095a89b0953
vinayakd282007-rgb <vinayakd282007@gmail.com>
2026-07-12 00:33:33 +0530

    Fix formatting and clean up print statements

--- day 10/10.py
+++ day 10/10.py
@@ -1,29 +1,29 @@
 #f for loops
 '''In Python, a for loop is used to iterate over a sequence 
 (like a list, tuple, string, or range) and execute a block of code repeatedly for each element in the sequence.'''
-'''i=1
+i=1
 for i in range(1,11,2): #(start , stop ,step)
-    print(i , end="  ")'''
+    print(i , end="  ")
 
-'''bag = ["red", "green" , "blue"]
+bag = ["red", "green" , "blue"]
 for ball in bag:
-    print(ball)'''
+    print(ball)
 
-"""name = "vinayak"
+name = "vinayak"
 for index , letter in enumerate(name):
-    print(letter*(index+1))"""
+    print(letter*(index+1))
 
-"""l = [12 , 32 , 42 , 52]
+l = [12 , 32 , 42 , 52]
 for  index , num in enumerate(l):
     print(f"{num} is in {index}th index")
     if num==42:
         print("Found 42")
 else:
-    print("All printed")"""
+    print("All printed")
 
-'''v = {"name": "vinayak" , "Age": "18" , "Height": "5.5"}
+v = {"name": "vinayak" , "Age": "18" , "Height": "5.5"}
 for key , values in v.items():
-    print(key ," " , values)'''
+    print(key ," " , values)
 
 #nested for loops
 for i in range(2,11):
